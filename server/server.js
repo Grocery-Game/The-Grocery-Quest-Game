2cc72b90fdcf944615c1c791fb15dbef4aea0844
Melody Yun Chen <Melodyyunc@gmail.com>
2020-07-10 10:42:27 -0400

    created server folder

--- server/server.js
+++ server/server.js
@@ -25,9 +25,9 @@ const port = process.env.PORT || 5000;
 
 // serve static build files while in propsData
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static('client/build'));
+  app.use(express.static('../client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve('./', 'client', 'build', 'index.html'))
   } )
 }
 
